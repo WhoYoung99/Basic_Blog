9e65286246183b226d9889d00fecfdacc349f59d
WhoYoung99 <linking2046.cv97@g2.nctu.edu.tw>
2016-07-05 21:07:58 +0800

    Fix css not loading problem (path setting inside app.yaml)

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ class Handler(webapp2.RequestHandler):
 
 class MainHandler(Handler):
     def get(self):
-        self.write("demon in the head.")
+        self.render("blog.html")
 
 app = webapp2.WSGIApplication([('/', MainHandler),
                                 ],
